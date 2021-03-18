136790c20c73d106b835fe7aa352330ff252b3b2
PauloZM <Munoz.pablito@Gmail.com>
2021-03-18 09:49:40 +0100

    Initial commit

---
+++ WpfAppOEF25.1/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppOEF25._1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            rbSamsung.IsChecked = true;
+        }
+        TV newTV = new TV();
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            lblTVgegevens.Content = "Merk:Samsung" + Environment.NewLine +
+                "Type: 85Q980TS" + Environment.NewLine + "Herz: 100" + Environment.NewLine + "Beeldgrootte: 85";
+            string packUri = "Images/400x320_Samsung.jpg";
+            imgTV.Source = new ImageSourceConverter().ConvertFromString(packUri) as ImageSource;
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            lblTVgegevens.Content = "Merk:Sony" + Environment.NewLine +
+                "Type: KD-77AG9" + Environment.NewLine + "Herz: 100" + Environment.NewLine + "Beeldgrootte: 77";
+            string packUri = "Images/Sony.jfif";
+            imgTV.Source = new ImageSourceConverter().ConvertFromString(packUri) as ImageSource;
+        }
+
+        private void cbPower_Checked(object sender, RoutedEventArgs e)
+        {
+            newTV.Power = true;
+            txtKanaal.Text = newTV.Kanaal.ToString();
+            txtVolume.Text = newTV.Volume.ToString();
+        }
+        private void cbPower_Unchecked(object sender, RoutedEventArgs e)
+        {
+            newTV.Power = false;
+            txtKanaal.Text = "";
+            txtVolume.Text = "";
+        }
+
+        private void btnKanaalPlus_Click(object sender, RoutedEventArgs e)
+        {
+            if (newTV.Power)
+            {
+                newTV.Kanaal++;
+                txtKanaal.Text = newTV.Kanaal.ToString();
+            }
+        }
+        private void btnKanaalMin_Click(object sender, RoutedEventArgs e)
+        {
+            if (newTV.Power)
+            {
+                newTV.Kanaal--;
+                //if (newTV.Kanaal < 0)
+                //{
+                //    newTV.Kanaal = 0;
+                //}
+                txtKanaal.Text = newTV.Kanaal.ToString();
+            }
+        }
+
+        private void btnVolumeMin_Click(object sender, RoutedEventArgs e)
+        {
+            if (newTV.Power)
+            {
+                newTV.Volume--;
+                //if (newTV.Volume < 0)
+                //{
+                //    newTV.Volume = 0;
+                //}
+                txtVolume.Text = newTV.Volume.ToString();
+            }
+        }
+
+        private void btnVolumePlus_Click(object sender, RoutedEventArgs e)
+        {
+            if (newTV.Power)
+            {
+                newTV.Volume++;
+            }
+            txtVolume.Text = newTV.Volume.ToString();
+        }
+    }
+}
---
+++ WpfAppOEF25.1/TV.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppOEF25._1
+{
+    class TV
+    {
+        private string _afbeelding;
+        private string _merk;
+        private string _type;
+        private int _beeldgrootte;
+        private int _herz;
+        private int _kanaal;
+        private int _volume;
+        private bool _power;
+        private bool _teletekst;
+
+        ////public string Helderheid { get; set; }
+        //public int MyProperty { get;  set; }  //kleinste waarde: 0 en g...   
+        ////value trager dan 0
+        //// value sneller 250 
+        ////int kleinste waarde -10000000   grootste waarde 11000000000000
+
+        public string Afbeelding { get => _afbeelding; set => _afbeelding = value; }
+        public string Merk { get => _merk; set => _merk = value; }
+        public string Type { get => _type; set => _type = value; }
+        public int Beeldgrootte { get => _beeldgrootte; set => _beeldgrootte = value; }
+        public int Herz { get => _herz; set => _herz = value; }
+        public int Kanaal
+        {
+            get => _kanaal;
+            set
+            {
+                if (Power)
+                {
+                    if (value >= 0 && value <= 30)
+                    {
+                        _kanaal = value;
+                    }
+                    else
+                    {
+                        if (value > 30)
+                        {
+                            _kanaal = 30;
+                        }
+                        else if (value < 0)
+                        {
+                            _kanaal = 0;
+                        }
+                    }
+                }
+            }
+        }
+        public int Volume
+        {
+            get => _volume;
+            set
+            {
+                if (Power)
+                {
+                    if (value >= 0 && value <= 99)
+                    {
+                        _volume = value;
+                    }
+                    else
+                    {
+                        if (value > 99)
+                        {
+                            _volume = 99;
+                        }
+                        else if (value < 0)
+                        {
+                            _volume = 0;
+                        }
+                    }
+                }
+            }
+        }
+
+        public bool Power { get => _power; set => _power = value; }
+        public bool Teletekst { get => _teletekst; set => _teletekst = value; }
+
+        public TV()
+        {
+            Power = false;
+        }
+        public TV(string merk, string type, int herz, int beeldgrootte, string afbeelding)
+        {
+            Teletekst = _teletekst;
+            Power = _power;
+            Kanaal = _kanaal;
+            Volume = _volume;
+            Merk = merk;
+            Type = type;
+            Herz = herz;
+            Beeldgrootte = beeldgrootte;
+            Afbeelding = afbeelding;
+        }
+        public override string ToString()
+        {
+            return "";
+        }
+
+    }
+}
